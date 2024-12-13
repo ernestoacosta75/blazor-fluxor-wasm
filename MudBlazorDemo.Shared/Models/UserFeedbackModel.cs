432ba41166596d3636200eca5c4b68a095e8ea67
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2024-12-13 15:31:50 +0100

    Creating the EditForm for user feedback using MudBlazor library

--- MudBlazorDemo.Shared/Models/UserFeedbackModel.cs
+++ MudBlazorDemo.Shared/Models/UserFeedbackModel.cs
@@ -9,7 +9,10 @@ namespace MudBlazorDemo.Shared.Models
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; } = string.Empty;
 
-        [Required] public int Rating { get; set; } = 1;
+
+        [Range(1, 10)]
+        [Required] 
+        public int Rating { get; set; } = 1;
 
         [MaxLength(100)]
         public string Comment { get; set; } = string.Empty;
---
+++ MudBlazorDemo/MudBlazorDemo.Client/Features/UserFeedback/Pages/Feedback.razor.cs
@@ -0,0 +1,23 @@
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using MudBlazorDemo.Client.Features.UserFeedback.Store;
+using MudBlazorDemo.Shared.Models;
+
+namespace MudBlazorDemo.Client.Features.UserFeedback.Pages
+{
+    public partial class Feedback
+    {
+        [Inject]
+        public IState<UserFeedbackState> UserFeedbackState { get; set; }
+
+        [Inject]
+        public IDispatcher Dispatcher { get; set; }
+
+        private UserFeedbackModel model => UserFeedbackState.Value.Model;
+
+        private void OnValidSubmit()
+        {
+            Dispatcher.Dispatch(new UserFeedbackSubmitAction(model));
+        }
+    }
+}
